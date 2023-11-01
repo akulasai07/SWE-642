f89afff8ea3eda99009dd15bd472e6a17c481266
Akshath Phillips <akshathphillips@icloud.com>
2023-10-31 20:09:29 -0400

    HW3 - Init

---
+++ HW-3-Spring/src/main/java/edu/gmu/swe642/hw3/spring/HW3SpringApplication.java
@@ -0,0 +1,13 @@
+package edu.gmu.swe642.hw3.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HW3SpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HW3SpringApplication.class, args);
+    }
+
+}
---
+++ HW-3-Spring/src/test/java/edu/gmu/swe642/hw3/spring/HW3SpringApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.gmu.swe642.hw3.spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HW3SpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
