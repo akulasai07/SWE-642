edb7de225aa5823eb16e4047c04e9828d8e212cc
Akshath Phillips <akshathphillips@icloud.com>
2023-11-07 20:31:21 -0500

    Angular flesh minor

--- HW-3-Angular/src/app/app-routing.module.ts
+++ HW-3-Angular/src/app/app-routing.module.ts
@@ -1,10 +1,19 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
+import {AppComponent} from "./app.component";
+import {SurveyFormComponent} from "./survey-form/survey-form.component";
+import {SurveyListComponent} from "./survey-list/survey-list.component";
+import {WelcomeComponent} from "./welcome/welcome.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: WelcomeComponent},
+  {path: 'survey', component: SurveyFormComponent},
+  {path: 'list-surveys', component: SurveyListComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
--- HW-3-Angular/src/app/app.module.ts
+++ HW-3-Angular/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SurveyFormComponent } from './survey-form/survey-form.component';
+import { SurveyListComponent } from './survey-list/survey-list.component';
+import { WelcomeComponent } from './welcome/welcome.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SurveyFormComponent,
+    SurveyListComponent,
+    WelcomeComponent,
   ],
   imports: [
     BrowserModule,
